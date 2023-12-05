a464429541bd87f8b12c183cbb6cc54abab9593e
Aleksey Sabantsev <asabantsev@gmail.com>
2023-12-05 15:57:57 +0300

    Initial Commit

---
+++ Next Track/Next_TrackApp.swift
@@ -0,0 +1,17 @@
+//
+//  Next_TrackApp.swift
+//  Next Track
+//
+//  Created by Aleksey Sabantsev on 05.12.2023.
+//
+
+import SwiftUI
+
+@main
+struct Next_TrackApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
